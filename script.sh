23ccc2eeac3c4b05f333742183eb95efa15bf182
Marina.Pakhnevich <Marina.Pakhnevich@jetbrains.com>
2017-01-11 15:01:26 +0300

    SKIP-BUILD

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+declare -i waitP=$1
+if [ $waitP -lt 1 ] ; then
+	waitP=5
+	elif [ $waitP -gt 300 ] ; then 
+		waitP=300;
+fi
+
 declare -i count=10
 bc="master"
 echo I am running on $bc > `echo $bc| tr '/' '_'`.txt
@@ -11,8 +18,8 @@ echo $count ; let count=$count+1
 whoami
 echo $count ; let count=$count+1
 ls -latr
-echo I am tired, wanna sleep a bit
-sleep 5
+echo "I am tired, wanna sleep a bit ($waitP sec)"
+sleep $waitP
 echo $count ; let count=$count+1
 uname -a
 echo I am exitting now
